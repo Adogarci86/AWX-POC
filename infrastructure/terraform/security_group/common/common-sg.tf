f2b17265e3bc886eb8f9f931dadb0552334496a3
Adolfo Garcia <adgarcia@usm1t29nmgtfm.informatica.com>
2019-05-02 11:42:06 -0700

    Test dollar var

--- infrastructure/terraform/security_group/common/common-sg.tf
+++ infrastructure/terraform/security_group/common/common-sg.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-  region = "$${aws_region}"
+  region = "${var.aws_region}"
 }
 
 resource "aws_security_group" "sg_common" {
-  name        = "$${common_sg_name}"
+  name        = "${var.common_sg_name}"
   description = "Allow all inbound traffic"
-  vpc_id      = "$${vpc_id}"
+  vpc_id      = "${var.vpc_id}"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
-    cidr_blocks = ["$${cidr_blocks_mgmt}"]
+    cidr_blocks = ["${var.cidr_blocks_mgmt}"]
     description = "SSH Port"
   }
 
@@ -23,14 +23,14 @@ resource "aws_security_group" "sg_common" {
   }
 
   tags {
-    Name = "$${common_sg_name}"
-    APPLICATION_ENVIRONMENT = "$${application_environment}"
-    APPLICATION_ROLE = "$${application_role}"
-    BUSINESS_ENTITY = "$${business_entity}"
-    BUSINESS_UNIT = "$${business_unit}"
-    DOMAIN = "$${domain}"
-    POD_ENVIRONMENT = "$${pod_environment}"
-    OWNER_EMAIL = "$${owner_email}"
-    POD_NAME = "$${pod_name}"
+    Name = "${var.common_sg_name}"
+    APPLICATION_ENVIRONMENT = "${var.application_environment}"
+    APPLICATION_ROLE = "${var.application_role}"
+    BUSINESS_ENTITY = "${var.business_entity}"
+    BUSINESS_UNIT = "${var.business_unit}"
+    DOMAIN = "${var.domain}"
+    POD_ENVIRONMENT = "${var.pod_environment}"
+    OWNER_EMAIL = "${var.owner_email}"
+    POD_NAME = "${var.pod_name}"
   }
 }

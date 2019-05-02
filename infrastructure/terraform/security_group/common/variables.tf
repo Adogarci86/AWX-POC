475e071c692827b5fadb8461f7a8cf07250e12c2
Adolfo Garcia <adgarcia@usm1t29nmgtfm.informatica.com>
2019-05-02 11:52:57 -0700

    Adding Tags to variables

--- infrastructure/terraform/security_group/common/variables.tf
+++ infrastructure/terraform/security_group/common/variables.tf
@@ -16,3 +16,43 @@ variable "vpc_id" {
   description = "VPC ID"
   type        = "string"
 }
+
+variable "application_environment" {
+  description = "application_environment"
+  type        = "string"
+}
+
+variable "application_role" {
+  description = "application_role"
+  type        = "string"
+}
+
+variable "business_entity" {
+  description = "business_entity"
+  type        = "string"
+}
+
+variable "business_unit" {
+  description = "business_unit"
+  type        = "string"
+}
+
+variable "domain" {
+  description = "domain"
+  type        = "string"
+}
+
+variable "pod_environment" {
+  description = "pod_environment"
+  type        = "string"
+}
+
+variable "owner_email" {
+  description = "owner_email"
+  type        = "string"
+}
+
+variable "pod_name" {
+  description = "pod_name"
+  type        = "string"
+}
